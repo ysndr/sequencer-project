38f0ccedb8496e09a174ce73ce61970ca0d43396
ysander <me@ysndr.de>
2019-04-23 17:58:15 +0200

    (C): Add pacall task

---
+++ c-uebungen/src/pascal/pascal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+   
+    int depth = atoi(argv[1]);
+
+    for (int n = 0; n < depth; n++) {
+        for (int k = 0; k <= n; k++) {
+            printf("%d ", over(n,k));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+int over(int n, int k) {
+    return fak(n)/(fak(k)*fak(n-k));
+}
+
+int fak(int k) {
+    int result = 1;
+    for (int i = 1; i <= k; i++) {
+        result = result*i;
+    }
+    return result;
+}
