5277078b44804fd5b14413f659499887d7dce607
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2019-04-25 17:30:22 +0200

    (C): fix pointer spacing for alloc macro

--- c-uebungen/src/include/macros.h
+++ c-uebungen/src/include/macros.h
@@ -3,7 +3,7 @@
 ({\
     ARRAY = (TYPE **) malloc(sizeof(TYPE*)*DIM1 + DIM1*DIM2*sizeof(TYPE)); \
     for (int p = 0; p < DIM1; p++) { \
-         ARRAY[p] = (TYPE*) (ARRAY + DIM1 + p * DIM2);\
+         ARRAY[p] = (TYPE*) (ARRAY + DIM1) + p * DIM2;\
          for (int e = 0; e < DIM2; e++) {\
                 ARRAY[p][e] = INIT;\
          }\
