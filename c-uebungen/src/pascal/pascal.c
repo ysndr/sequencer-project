71df203772ce4ca1cded0d6b46f71e64cb128c74
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2019-04-25 17:30:47 +0200

    (C): Add Pascal task

--- c-uebungen/src/pascal/pascal.c
+++ c-uebungen/src/pascal/pascal.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "include/macros.h"
 
 int main(int argc, char const *argv[])
 {
    
-    int depth = atoi(argv[1]);
+   int depth = atoi(argv[1]);
 
-    for (int n = 0; n < depth; n++) {
-        for (int k = 0; k <= n; k++) {
-            printf("%d ", over(n,k));
+   int **table;
+   ALLOC_ARRAY2D(table, int, 12, 12, (0x00000000));
+
+    table[0][0] = 1;
+
+
+    for (int r = 1; r<depth; r++) {
+        for (int c = 0; c < r; c++) {
+            int first = table[r-1][c];
+            int second = (c > 0) ? table[r-1][c-1] : 0;
+            int new = first + second;
+
+            table[r][c] = new;
+            printf("%d ", new);
         }
         printf("\n");
     }
+
 
     return 0;
 }
 
 
-int over(int n, int k) {
-    return fak(n)/(fak(k)*fak(n-k));
-}
 
-int fak(int k) {
-    int result = 1;
-    for (int i = 1; i <= k; i++) {
-        result = result*i;
-    }
-    return result;
-}
+
