ec259ec7df62e3abd8cc7abbfada9c8660dcd96d
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2019-04-25 16:43:53 +0200

    (C): make includes work

---
+++ c-uebungen/src/include/macros.h
@@ -0,0 +1,13 @@
+
+#define ALLOC_ARRAY2D(ARRAY,TYPE,DIM1,DIM2,INIT) \
+({\
+    ARRAY = (TYPE **) malloc(sizeof(TYPE*)*DIM1 + DIM1*DIM2*sizeof(TYPE)); \
+    for (int p = 0; p < DIM1; p++) { \
+         ARRAY[p] = (TYPE*) (ARRAY + DIM1 + p * DIM2);\
+         for (int e = 0; e < DIM2; e++) {\
+                ARRAY[p][e] = INIT;\
+         }\
+    }\
+})
+
+#define DEL_BLOCK2D(ARRAY) (free(ARRAY))
--- c-uebungen/src/macros/macros.c
+++ c-uebungen/src/macros/macros.c
@@ -1,20 +1,7 @@
-#include "./include/macros.h"
+#include "include/macros.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define ALLOC_ARRAY2D(ARRAY,TYPE,DIM1,DIM2,INIT) \
-({\
-    ARRAY = (TYPE **) malloc(sizeof(TYPE*)*DIM1 + DIM1*DIM2*sizeof(TYPE)); \
-    for (int p = 0; p < DIM1; p++) { \
-         ARRAY[p] = (TYPE*) (ARRAY + DIM1 + p * DIM2);\
-         for (int e = 0; e < DIM2; e++) {\
-                ARRAY[p][e] = INIT;\
-         }\
-    }\
-})
-
-#define DEL_BLOCK2D(ARRAY) (free(ARRAY))
 
 
 int main(int argc, char const *argv[])
@@ -31,5 +18,6 @@ int main(int argc, char const *argv[])
             assert((feld[i][j] == 0xabcdef12));
 
     DEL_BLOCK2D(feld);
+    puts("success");
     return 0;
 }
